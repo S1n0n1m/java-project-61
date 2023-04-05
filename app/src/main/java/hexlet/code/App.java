9a520ed176bb7993d00b30c61e49bdd5deac5c5b
S1n0n1mPC <S1n0n1mpc@gmail.com>
2023-04-05 18:12:00 +0300

    add fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,39 +1,42 @@
 package hexlet.code;
 
+import java.util.Scanner;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.CalcGame;
 import hexlet.code.games.GcdGame;
 import hexlet.code.games.ProgressionGame;
 import hexlet.code.games.PrimeGame;
 
-import java.util.Scanner;
-
 public class App {
+    private static final String GREET = "1";
+    private static final String EVEN = "2";
+    private static final String CALC = "3";
+    private static final String GCD = "4";
+    private static final String PROGRESSION = "5";
+    private static final String PRIME = "6";
+    private static final String EXIT = "0";
     public static void main(String[] args) {
-
-        System.out.println("""
-                Please enter the game number and press Enter.
-                1 - Greet
-                2 - Even
-                3 - Calc
-                4 - GCD
-                5 - Progression
-                6 - Prime
-                0 - Exit""");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc ");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
         System.out.print("Your choice: ");
-
-        Scanner scanner = new Scanner(System.in);
-        int selectionGame = scanner.nextInt();
-
+        Scanner console = new Scanner(System.in);
+        String selectionGame = console.nextLine();
         switch (selectionGame) {
-            case 1 -> Cli.greeting();
-            case 2 -> EvenGame.launchEvenGame();
-            case 3 -> CalcGame.launchCalcGame();
-            case 4 -> GcdGame.launchGcdGame();
-            case 5 -> ProgressionGame.launchProgressionGame();
-            case 6 -> PrimeGame.launchPrimeGame();
-            case 0 -> System.out.println("Bye!");
-            default -> throw new IllegalStateException("Unexpected value: " + selectionGame);
+            case GREET -> Cli.greeting();
+            case EVEN -> EvenGame.launchEvenGame();
+            case CALC -> CalcGame.launchCalcGame();
+            case GCD -> GcdGame.launchGcdGame();
+            case PROGRESSION -> ProgressionGame.launchProgressionGame();
+            case PRIME -> PrimeGame.launchPrimeGame();
+            case EXIT -> System.out.println("Goodbye!");
+            default -> throw new IllegalStateException("Unexpected values" + selectionGame);
         }
+
     }
 }
--- app/src/main/java/hexlet/code/games/CalcGame.java
+++ app/src/main/java/hexlet/code/games/CalcGame.java
@@ -6,23 +6,25 @@ public class CalcGame {
     private static final int QUESTIONS_COUNT = 3;
     private static final int MAX_RANDOM_NUMBER = 10;
     private static final String[][] QUESTIONS_ANSWER = new String[QUESTIONS_COUNT][2];
-    private final static char[] MATH_OPERATORS = {'*', '-', '+'};
     private static final int QUESTIONS_NUMBER = 0;
     private static final int ANSWER_NUMBER = 1;
     public static final String GAME_RULES = "What is the result of the expression?";
 
     public static void launchCalcGame() {
+        char[] mathOperators = {'*', '-', '+'};
         for (int i = 0; i < QUESTIONS_COUNT; i++) {
             int firstNum = (int) (Math.random() * MAX_RANDOM_NUMBER - 1);
             int secondNum = (int) (Math.random() * MAX_RANDOM_NUMBER - 1);
-            int randomOperators = (int) (Math.random() * MATH_OPERATORS.length);
-            char operator = MATH_OPERATORS[randomOperators];
+            int randomOperators = (int) (Math.random() * mathOperators.length);
+            char operator = mathOperators[randomOperators];
+            int answer;
             switch (operator) {
-                case '*' -> QUESTIONS_ANSWER[i][ANSWER_NUMBER] = String.valueOf(firstNum * secondNum);
-                case '-' -> QUESTIONS_ANSWER[i][ANSWER_NUMBER] = String.valueOf(firstNum - secondNum);
-                case '+' -> QUESTIONS_ANSWER[i][ANSWER_NUMBER] = String.valueOf(firstNum + secondNum);
+                case '*' -> answer = firstNum * secondNum;
+                case '-' -> answer = firstNum - secondNum;
+                case '+' -> answer = firstNum + secondNum;
                 default -> throw new IllegalStateException("Unexpected value: " + operator);
             }
+            QUESTIONS_ANSWER[i][ANSWER_NUMBER] = String.valueOf(answer);
             QUESTIONS_ANSWER[i][QUESTIONS_NUMBER] = firstNum + " " + operator + " " + secondNum;
         }
         Engine.launchGame(GAME_RULES, QUESTIONS_ANSWER);
